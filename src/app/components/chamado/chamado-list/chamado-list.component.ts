f59b0e3efd9d1e1a681ebccf873a8cd34888988c
Luan Lima <lordellu@hotmail.com>
2022-07-04 20:31:19 -0300

    filtro e metodo findAll para chamado finalizado

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,3 +1,4 @@
+import { ChamadoService } from './../../../services/chamado.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from './../../../models/chamado';
@@ -11,38 +12,64 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class ChamadoListComponent implements OnInit {
 
   checked: false;
-  
-  ELEMENT_DATA: Chamado[] = [
-    {
-      id: 1,
-      dataAbertura: '21/06/2022', 
-      dataFechamento: '23/06/2022', 
-      prioridade: 'ALTA',
-      status: 'ANDAMENTO',
-      titulo: 'Remover acesso',
-      observacoes: 'Remover acesso do usuário XYP',
-      tecnico: 1,
-      cliente: 6,
-      nomeCliente: 'Luan Lordello',
-      nomeTecnico: 'Albert Esteein'
-  }
-]
+
+  ELEMENT_DATA: Chamado[] = []
+  FILTERED_DATA: Chamado[] = []
 
   displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico','dataAbertura', 'prioridade' , 'status' , 'acoes']
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor() { }
+  constructor(private service: ChamadoService) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
-
+  findAll(): void{
+    this.service.findAll().subscribe(response => {
+      this.ELEMENT_DATA = response;
+      this.dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 
    // pega o valor digitado no campo de filtro
    applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     // Chama o metodo para filtrar nos dados da nossa tabela antes, tira espaço em branco e tudo fica caixa baixa
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatusString(status: any): string {
+    if(status == '0'){
+      return 'Aberto'
+    } else if(status == '1'){
+      return 'Andamento'
+    } else{
+      return 'Encerrado'
+    }
+  }
+
+    retornaPrioridadeString(prioridade: any): string {
+      if(prioridade == '0'){
+        return 'Baixa'
+      } else if(prioridade == '1'){
+        return 'Média'
+      } else{
+        return 'Alta'
+      }
+  }
+
+  ordenaPorStatus(status: any): void{
+    let list: Chamado[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status == status){
+        list.push(element);
+      }
+    })
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
   }
 }
---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Chamado } from './../models/chamado';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${environment.urlAPI}/chamados`);
+  }
+}
